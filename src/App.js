f0ba8d21eac2a06746f275accc938e2ab8c2241f
Mrad Mohamed <mouhamed.mrad94@gmail.com>
2020-09-21 11:56:37 +0100

    implement react helmet async

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import Header from "./components/Header";
 
-import { GlobalStyle, AppWrapper } from "./styles";
+import { AppWrapper, GlobalStyle } from "./styles";
 
 export default function App() {
   return (
-    <AppWrapper>
-      <GlobalStyle />
-      <Header />
-      <section>
-        <div className="shadow" />
-        <div className="container">
-          <div className="content opacity">
-            <h3 className="title">
-              About
-              <div className="border" />
-            </h3>
-            <p className="text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
-              officiis quos expedita ipsa, a quidem inventore voluptates debitis
-              accusamus tenetur qui et voluptas dicta, culpa earum, doloribus
-              odio consectetur consequuntur soluta quasi nobis! Deserunt
-              voluptatum reiciendis iure expedita sequi quisquam laboriosam
-              temporibus exercitationem.
-            </p>
+    <HelmetProvider>
+      <AppWrapper>
+        <Helmet>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+            rel="stylesheet"
+          />
+        </Helmet>
+        <GlobalStyle />
+        <Header />
+        <section>
+          <div className="shadow" />
+          <div className="container">
+            <div className="content opacity">
+              <h3 className="title">
+                About
+                <div className="border" />
+              </h3>
+              <p className="text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque
+                officiis quos expedita ipsa, a quidem inventore voluptates
+                debitis accusamus tenetur qui et voluptas dicta, culpa earum,
+                doloribus odio consectetur consequuntur soluta quasi nobis!
+                Deserunt voluptatum reiciendis iure expedita sequi quisquam
+                laboriosam temporibus exercitationem.
+              </p>
+            </div>
+            <div className="imgContainer opacity">
+              <img src="img/image.jpg" alt="" />
+            </div>
           </div>
-          <div className="imgContainer opacity">
-            <img src="img/image.jpg" alt="" />
-          </div>
-        </div>
-      </section>
-    </AppWrapper>
+        </section>
+      </AppWrapper>
+    </HelmetProvider>
   );
 }
