a77386020d73039f52fc1623e0c64072bf22e048
Mrad Mohamed <mouhamed.mrad94@gmail.com>
2020-09-23 17:17:03 +0100

    nav

--- src/styles.js
+++ src/styles.js
@@ -18,6 +18,17 @@ export const HeaderWrapper = styled.div`
   position: relative;
   overflow: hidden;
 
+  &:after {
+    content: " 🦄";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    background-color: rgba(0, 0, 0, 0.05);
+    z-index: 25;
+  }
+
   .mountain1 {
     position: absolute;
     bottom: -100px;
@@ -55,6 +66,16 @@ export const HeaderWrapper = styled.div`
     width: 2100px;
     bottom: 250px;
     right: 0;
+  }
+
+  nav {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 1rem 0;
+    z-index: 30;
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0.05), transparent);
   }
 `;
 
